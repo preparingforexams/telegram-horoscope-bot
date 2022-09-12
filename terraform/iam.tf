76a41f3d73d22972f6c11465ac431f4f3ec78f8c
Björn Petersen <git@bjoernpetersen.net>
2022-09-12 11:20:16 +0200

    Remove broken condition

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -7,9 +7,4 @@ resource "google_project_iam_member" "service_account_publisher" {
   project = google_service_account.service_account.project
   role    = "roles/pubsub.publisher"
   member  = "serviceAccount:${google_service_account.service_account.email}"
-
-  condition {
-    title      = "Cancer Topic"
-    expression = "resource.name == '${google_pubsub_topic.horoscopes.id}'"
-  }
 }
